5bdcffe43e449ecd8b8f9e200108e1f63280e78d
Andreas Eger <dev@eger-andreas.de>
2015-11-26 14:26:29 +0100

    tag types

--- app/models/point_of_interest.rb
+++ app/models/point_of_interest.rb
@@ -2,4 +2,16 @@ class PointOfInterest < ActiveRecord::Base
   has_and_belongs_to_many :tags
   belongs_to :city
   has_one :country, through: :city
+
+  def phones
+    Array(contact['phones'])
+  end
+
+  def mails
+    Array(contact['mails'])
+  end
+
+  def web_address
+    contact['web_address']
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,18 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :points_of_interest
+
+  has_many :sub_tags, class_name: "Tag", foreign_key: 'parent_id'
+  belongs_to :parent, class_name: "Tag"
+
+  def type
+    case parent_id
+    when 0
+      :main_category
+    when nil
+      :tag
+    else
+      :sub_category
+    end
+  end
+
 end
