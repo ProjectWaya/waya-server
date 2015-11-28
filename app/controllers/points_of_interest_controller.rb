3b3b2958f73d695ab8076d8c7bbcb4228406ca12
Yohan Robert <yor@runtastic.com>
2015-11-28 15:33:22 +0100

    Added error for missing city filter parameter

--- app/controllers/points_of_interest_controller.rb
+++ app/controllers/points_of_interest_controller.rb
@@ -5,13 +5,45 @@ class PointsOfInterestController < ApplicationController
   # GET /points_of_interest.json
   def index
     included = params[:include].try(:split, ",")
-    filter = params[:filter] || {}
+    city_id = filter[:city]
+    if city_id
+      render json: fetch_pois(city_id), include: included
+    else
+      render json: city_id_missing_error, adapter: :json
+    end
+  end
+
+  private
+
+  def filter
+    @filter ||= params[:filter] || {}
+  end
+
+  def tags_ids
+    tags_ids = filter[:tag]
+    tags_ids = tags_ids.try(:split, ",") unless tags_ids.is_a?(Array)
+  end
+ 
+  def fetch_pois(city_id)
     ids = PointOfInterest.select(:id)
       .joins(:city, :country, :tags)
-      .where(cities: { id: filter[:city] })
-    tags_ids = filter[:tag].try(:split, ",")
+      .where(cities: { id: city_id })
     ids = ids.where(tags: { id: tags_ids }) unless tags_ids.blank?
-    points_of_interest = PointOfInterest.eager_load(:city, :country, :tags).where(id: ids)
-    render json: points_of_interest, include: included
+    PointOfInterest.eager_load(:city, :country, :tags).where(id: ids)
+  end
+
+  def city_id_missing_error
+    {
+      errors: [
+        {
+         status: "400",
+         code: "MANDATORY_QUERY_PARAMETER_MISSING",
+         detail: "filter[country] query parameter is missing",
+         source: {
+           parameter: "filter[country]"
+         }
+        }
+      ]
+    }
   end
 end
