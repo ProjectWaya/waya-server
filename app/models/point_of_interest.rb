80b60df8d90510a18de03b57f6665c73c2ec7bfb
Bibek Shrestha <bibek.shrestha@runtastic.com>
2016-01-28 14:41:12 +0100

    Revert back model/poi.create method

--- app/models/point_of_interest.rb
+++ app/models/point_of_interest.rb
@@ -3,21 +3,4 @@ class PointOfInterest < ActiveRecord::Base
   has_and_belongs_to_many :tags
   belongs_to :city
   has_one :country, through: :city
-
-  def self.create(opts)
-  	if opts.include?(:tags)
-  	  tags = opts.fetch(:tags)
-  	  
-  	  new_tags = []
-  	  tags.each do |tag|
-  	  	if tag.parent_id != 0
-  	  		unless tags.map(&:id).include?(tag.parent_id) || new_tags.map(&:id).include?(tag.parent_id)
-  	  		  new_tags << Tag.find(tag.parent_id)
-  	  		end
-  	  	end
-  	  end
-  	  tags.concat(new_tags)
-  	end
-  	super(opts)
-  end
 end
