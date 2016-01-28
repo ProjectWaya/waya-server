3a3119bdea09e86a1cdce7672105813919793821
Bibek Shrestha <bibek.shrestha@runtastic.com>
2016-01-28 10:09:17 +0100

    Redirect by default to waya

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
 
   get '*path', to: 'ember#index', constraints: { format: 'html' }
   get '/',     to: 'ember#index', constraints: { format: 'html' }
+  root 'ember#index'
 end
